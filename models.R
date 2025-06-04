38cf25e0d4855507585a2725c14ecab080734715
federicohorl <federicohorl1@gmail.com>
2025-06-04 10:00:41 -0300

    Modelado

--- models.R
+++ models.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 taxis = read_csv('taxis.csv')
+taxis = taxis |> mutate(hour = as.integer(hour(tpep_pickup_datetime)))
 
 mod0 = lm(formula = total_amount ~ trip_distance, data = taxis)
 summary(mod0)
@@ -8,11 +9,21 @@ summary(mod0)
 mod1 = lm(formula = total_amount ~ trip_distance + trip_duration, data = taxis)
 summary(mod1)
 
-taxis = taxis |> mutate(hour = as.integer(hour(tpep_pickup_datetime)))
-mod2 = lm(formula = total_amount ~ trip_distance * factor(hour), data = taxis)
+ggplot(taxis)+
+  geom_col(aes(x = hour, y = mean(total_amount)))
+mod2 = lm(formula = total_amount ~ trip_distance + factor(hour), data = taxis)
 summary(mod2)
 
-mod3 = lm(formula = total_amount ~ trip_distance + PUBorough, data = taxis)
+mod2_2 = lm(formula = total_amount ~ trip_distance + poly(hour, 3), data = taxis)
+summary(mod2_2)
+
+mod3 = lm(formula = total_amount ~ trip_distance + trip_duration + PUBorough, data = taxis)
 summary(mod3)
 
-anova(mod0, mod1, mod2)
+mod4 = lm(formula = total_amount ~ trip_distance + trip_duration + PUBorough + hour, data = taxis)
+summary(mod4)
+
+mod5 = lm(formula = total_amount ~ trip_distance + trip_duration + PUBorough + factor(hour), data = taxis)
+summary(mod5)
+
+anova(mod1, mod3, mod5)
