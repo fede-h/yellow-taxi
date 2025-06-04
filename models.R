bfd3b4a44c226c0cc78d29fb1530118aee7c286b
federicohorl <federicohorl1@gmail.com>
2025-06-04 09:23:40 -0300

    Modelado lineal

--- models.R
+++ models.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 
 taxis = read_csv('taxis.csv')
-taxis = drop_na(taxis)
 
 mod0 = lm(formula = total_amount ~ trip_distance, data = taxis)
 summary(mod0)
@@ -9,7 +8,11 @@ summary(mod0)
 mod1 = lm(formula = total_amount ~ trip_distance + trip_duration, data = taxis)
 summary(mod1)
 
-mod2 = lm(formula = total_amount ~ trip_distance + trip_duration + PUBorough, data = taxis)
+taxis = taxis |> mutate(hour = as.integer(hour(tpep_pickup_datetime)))
+mod2 = lm(formula = total_amount ~ trip_distance * factor(hour), data = taxis)
 summary(mod2)
+
+mod3 = lm(formula = total_amount ~ trip_distance + PUBorough, data = taxis)
+summary(mod3)
 
 anova(mod0, mod1, mod2)
